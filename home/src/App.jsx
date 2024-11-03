3fd79ffb3bad3b2e6dd1fb8121f10b21a267dda7
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-11-03 15:17:19 +0300

    add HomeContent component

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -5,11 +5,12 @@ import "./index.scss";
 
 import Header from "./Header";
 import Footer from "./Footer";
+import HomeContent from "./HomeContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header app={{ name: "home" }} />
-    <div className="my-10">Home Page Content</div>
+    <HomeContent />
     <Footer />
   </div>
 );
